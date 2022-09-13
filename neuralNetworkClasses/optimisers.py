d6b252455d63f6e6cddb492155d146626d663d69
kjw009 <kjw007@icloud.com>
2022-09-13 18:14:07 +0100

    Fixed optimiser bugs

--- neuralNetworkClasses/optimisers.py
+++ neuralNetworkClasses/optimisers.py
@@ -4,19 +4,19 @@ import numpy as np
 # SGD optimiser class
 class Optimizer_SGD:
     # Initialise optimiser and set default params
-    def __init__(self, learning_rate = 1.0, decay = 0.0, momentum = 0.0):
+    def __init__(self, learning_rate=1.0, decay=0.0, momentum=0.0):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
-        self.decay = decay 
+        self.decay = decay
         self.iterations = 0
         self.momentum = momentum
-    
+
     # Call once before any parameter updates
     def pre_update_params(self):
         # If decay argument, apply decay to current learning rate calculations
         self.current_learning_rate = self.learning_rate * \
-                                     (1.0 / (1.0 + self.decay * self.iterations))
-    
+            (1.0 / (1.0 + self.decay * self.iterations))
+
     # Update parameters
     def update_params(self, layer):
         # If momentum was passed
@@ -25,27 +25,27 @@ class Optimizer_SGD:
             if not hasattr(layer, 'weight_momentums'):
                 layer.weight_momentums = np.zeros_like(layer.weights)
                 layer.bias_momentums = np.zeros_like(layer.biases)
-                
-                weight_updates = \
-                                 self.momentum * layer.weight_momentums - \
-                                 self.current_learning_rate * layer.dweights
-                layer.weight_momentums = weight_updates
-                
-                # Update bias
-                bias_updates = self.momentum * layer.bias_momentums - \
-                               self.current_learning_rate * layer.dbiases
-                layer.bias_momentums = bias_updates
-                
-                # If no momentum is used 
-            else:
-                weight_updates = -self.current_learning_rate * \
-                                  layer.dweights
-                bias_updates = -self.current_learning_rate * layer.dbiases
-                
-            # Update weights and biases
-            layer.weights += weight_updates
-            layer.biases += bias_updates
-        
+
+            weight_updates = \
+                self.momentum * layer.weight_momentums - \
+                self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            # Update bias
+            bias_updates = self.momentum * layer.bias_momentums - \
+                self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        # If no momentum is used
+        else:
+            weight_updates = -self.current_learning_rate * \
+                layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        # Update weights and biases
+        layer.weights += weight_updates
+        layer.biases += bias_updates
+
     # Call once after a params update
     def post_update_params(self):
-        self.iterations += 1             
+        self.iterations += 1
